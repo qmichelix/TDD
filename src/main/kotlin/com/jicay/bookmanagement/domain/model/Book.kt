4ad5383fcbf155fdd51a9fb73027cc5fd6598698
qmichelix <109591838+qmichelix@users.noreply.github.com>
2023-12-11 10:38:59 +0100

    Update Book.kt

--- src/main/kotlin/com/jicay/bookmanagement/domain/model/Book.kt
+++ src/main/kotlin/com/jicay/bookmanagement/domain/model/Book.kt
@@ -1,3 +1,8 @@
 package com.jicay.bookmanagement.domain.model
 
-data class Book(val name: String, val author: String)
+class Book(
+    val id: Long,
+    val name: String,
+    val author: String,
+    var isReserved: Boolean // Assurez-vous que cette propriété existe
+)
