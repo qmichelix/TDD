a33cb3d2c237c138697b0ffa6eab4d9bcdac4c0a
qmichelix <109591838+qmichelix@users.noreply.github.com>
2023-12-11 10:44:44 +0100

    Update BookDTO.kt

--- src/main/kotlin/com/jicay/bookmanagement/infrastructure/driver/web/dto/BookDTO.kt
+++ src/main/kotlin/com/jicay/bookmanagement/infrastructure/driver/web/dto/BookDTO.kt
@@ -2,16 +2,25 @@ package com.jicay.bookmanagement.infrastructure.driver.web.dto
 
 import com.jicay.bookmanagement.domain.model.Book
 
-data class BookDTO(val name: String, val author: String) {
+data class BookDTO(
+    val id: Long, // Ajoutez l'ID du livre
+    val name: String,
+    val author: String,
+    val isReserved: Boolean // Ajoutez l'état de réservation du livre
+) {
     fun toDomain(): Book {
         return Book(
+            id = this.id,
             name = this.name,
-            author = this.author
+            author = this.author,
+            isReserved = this.isReserved
         )
     }
 }
 
 fun Book.toDto() = BookDTO(
+    id = this.id,
     name = this.name,
-    author = this.author
+    author = this.author,
+    isReserved = this.isReserved
 )
