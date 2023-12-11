69169a6fe0378efc0dd64d74687e6ab1805f8924
qmichelix <109591838+qmichelix@users.noreply.github.com>
2023-12-11 09:54:42 +0100

    Update BookController.kt

--- src/main/kotlin/com/jicay/bookmanagement/infrastructure/driver/web/BookController.kt
+++ src/main/kotlin/com/jicay/bookmanagement/infrastructure/driver/web/BookController.kt
@@ -3,7 +3,7 @@ package com.jicay.bookmanagement.infrastructure.driver.web
 import com.jicay.bookmanagement.domain.usecase.BookUseCase
 import com.jicay.bookmanagement.infrastructure.driver.web.dto.BookDTO
 import com.jicay.bookmanagement.infrastructure.driver.web.dto.toDto
-import org.springframework.http.HttpStatus
+import org.springframework.http.ResponseEntity
 import org.springframework.web.bind.annotation.*
 
 @RestController
@@ -23,10 +23,20 @@ class BookController(
     @ResponseStatus(HttpStatus.CREATED)
     fun addBook(@RequestBody bookDTO: BookDTO) {
         bookUseCase.addBook(bookDTO.toDomain())
+    }
+
     @CrossOrigin
-        @PostMapping("/{id}/reserve")
-        fun reserveBook(@PathVariable id: Long): ResponseEntity<Void> {
+    @PostMapping("/{id}/reserve")
+    fun reserveBook(@PathVariable id: Long): ResponseEntity<Void> {
+        return try {
             bookUseCase.reserveBook(id)
-            return ResponseEntity.ok().build()
+            ResponseEntity.ok().build()
+        } catch (e: BookAlreadyReservedException) {
+            ResponseEntity.status(HttpStatus.CONFLICT).build() // 409 Conflict
+        } catch (e: BookNotFoundException) {
+            ResponseEntity.status(HttpStatus.NOT_FOUND).build() // 404 Not Found
+        } catch (e: Exception) {
+            ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR).build() // 500 Internal Server Error
+        }
     }
 }
