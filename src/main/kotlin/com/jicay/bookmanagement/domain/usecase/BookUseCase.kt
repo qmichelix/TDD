311a3c074742b75353f9cc24aef50867cf95aae6
qmichelix <109591838+qmichelix@users.noreply.github.com>
2023-12-11 09:59:28 +0100

    Update BookUseCase.kt

--- src/main/kotlin/com/jicay/bookmanagement/domain/usecase/BookUseCase.kt
+++ src/main/kotlin/com/jicay/bookmanagement/domain/usecase/BookUseCase.kt
@@ -2,6 +2,8 @@ package com.jicay.bookmanagement.domain.usecase
 
 import com.jicay.bookmanagement.domain.model.Book
 import com.jicay.bookmanagement.domain.port.BookPort
+import com.jicay.bookmanagement.domain.exceptions.BookAlreadyReservedException
+import com.jicay.bookmanagement.domain.exceptions.BookNotFoundException
 
 class BookUseCase(
     private val bookPort: BookPort
@@ -14,5 +16,14 @@ class BookUseCase(
 
     fun addBook(book: Book) {
         bookPort.createBook(book)
+    }
+
+     fun reserveBook(bookId: Long) {
+        val book = bookPort.getBookById(bookId) ?: throw BookNotFoundException()
+        if (book.isReserved) {
+            throw BookAlreadyReservedException()
+        }
+        book.isReserved = true
+        bookPort.updateBook(book)
     }
 }
