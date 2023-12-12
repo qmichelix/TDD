eef33f8993724d5b385d51d1e58be1ba05d1153e
qmichelix <109591838+qmichelix@users.noreply.github.com>
2023-12-12 15:25:14 +0100

    Update BookStepDefs.kt

--- src/testComponent/kotlin/com/jicay/bookmanagement/BookStepDefs.kt
+++ src/testComponent/kotlin/com/jicay/bookmanagement/BookStepDefs.kt
@@ -9,30 +9,26 @@ import io.cucumber.java.en.When
 import io.restassured.RestAssured
 import io.restassured.RestAssured.given
 import io.restassured.http.ContentType
-import io.restassured.path.json.JsonPath
-import io.restassured.response.ValidatableResponse
+import io.restassured.response.Response
 import org.springframework.boot.test.web.server.LocalServerPort
 
 class BookStepDefs {
     @LocalServerPort
     private var port: Int? = 0
 
-    companion object {
-        var lastBookResult: ValidatableResponse? = null
-    }
+    private var lastResponse: Response? = null
 
     @Before
     fun setup(scenario: Scenario) {
         RestAssured.baseURI = "http://localhost:$port"
         RestAssured.enableLoggingOfRequestAndResponseIfValidationFails()
-        lastBookResult = null
+        lastResponse = null
     }
 
     @When("the user creates the book {string} written by {string}")
     fun createBook(title: String, author: String) {
-        given()
+        lastResponse = given()
             .contentType(ContentType.JSON)
-            .and()
             .body(
                 """
                     {
@@ -41,48 +37,33 @@ class BookStepDefs {
                     }
                 """.trimIndent()
             )
-            .`when`()
             .post("/books")
-            .then()
-            .statusCode(201)
+            .andReturn()
+        assertThat(lastResponse?.statusCode).isEqualTo(201)
     }
 
     @When("the user get all books")
     fun getAllBooks() {
-        lastBookResult = given()
-            .`when`()
-            .get("/books")
-            .then()
-            .statusCode(200)
+        lastResponse = given().get("/books").andReturn()
+        assertThat(lastResponse?.statusCode).isEqualTo(200)
     }
 
     @When("the user reserves the book with id {long}")
     fun reserveBook(bookId: Long) {
-        lastBookResult = given()
-            .`when`()
-            .post("/books/$bookId/reserve")
-            .then()
-            .statusCode(200)
+        lastResponse = given().post("/books/$bookId/reserve").andReturn()
+        assertThat(lastResponse?.statusCode).isEqualTo(200)
     }
 
     @Then("the book {string} should be marked as reserved")
     fun checkBookIsReserved(title: String) {
-        val book = lastBookResult?.extract()?.jsonPath()?.getJsonObject("find { it.name == '$title' }")
+        val books = lastResponse?.jsonPath()?.getList<Map<String, Any>>("")
+        val book = books?.find { it["name"] == title }
         assertThat(book?.get("isReserved")).isEqualTo(true)
     }
 
     @Then("the list should contains the following books in the same order")
-    fun shouldHaveListOfBooks(payload: List<Map<String, Any>>) {
-        val expectedResponse = payload.joinToString(separator = ",", prefix = "[", postfix = "]") { line ->
-            """
-                ${
-                    line.entries.joinToString(separator = ",", prefix = "{", postfix = "}") {
-                        """"${it.key}": "${it.value}""""
-                    }
-                }
-            """.trimIndent()
-        }
-        assertThat(lastBookResult?.extract()?.body()?.jsonPath()?.prettify())
-            .isEqualTo(JsonPath(expectedResponse).prettify())
+    fun shouldHaveListOfBooks(expectedBooks: List<Map<String, Any>>) {
+        val actualBooks = lastResponse?.jsonPath()?.getList<Map<String, Any>>("")
+        assertThat(actualBooks).isEqualTo(expectedBooks)
     }
 }
